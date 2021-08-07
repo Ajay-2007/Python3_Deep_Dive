3fb8a4b644db3c4c7c28cee98693cc5d7c27f566
Ajay-2007 <iit2017040@iiita.ac.in>
2021-08-07 21:29:57 +0530

    added more tests

--- pytest/test-automation-with-pytest/tests.py
+++ pytest/test-automation-with-pytest/tests.py
@@ -39,3 +39,71 @@ class ClassTest(object):
             ), "Listing a folder with one file did not return expected result!"
         finally:
             shutil.rmtree("/tmp/testfolder")
+
+    @staticmethod
+    def test_list_multiple_files():
+        """test simple ls
+
+        Returns:
+            [type]: [description]
+        """
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/second.doc").touch()
+            result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
+            print("Result: [{}]".format(result))
+            assert "first.txt" in str(
+                result.stdout
+            ), "Listing a folder with multiple files did not return expected result!"
+            assert "second.doc" in str(
+                result.stdout
+            ), "Listing a folder with multiple files did not return expected result!"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
+
+    @staticmethod
+    def test_hidden_files():
+        """test simple ls
+
+        Returns:
+            [type]: [description]
+        """
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/.hidden_file").touch()
+            result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
+            print("Result: [{}]".format(result))
+            assert "first.txt" in str(
+                result.stdout
+            ), "Listing a folder with hidden file did not return expected result!"
+            assert ".hidden_file" not in str(
+                result.stdout
+            ), "ls listed hidden file when it shouldn't have!"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
+
+    @staticmethod
+    def test_list_hidden_files():
+        """test simple ls
+
+        Returns:
+            [type]: [description]
+        """
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/.hidden_file").touch()
+            result = subprocess.run(
+                ["ls", "-a", "/tmp/testfolder"], stdout=subprocess.PIPE
+            )
+            print("Result: [{}]".format(result))
+            assert "first.txt" in str(
+                result.stdout
+            ), "Listing a folder with hidden file did not return expected result!"
+            assert ".hidden_file" in str(
+                result.stdout
+            ), "ls listed hidden file when it shouldn't have!"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
