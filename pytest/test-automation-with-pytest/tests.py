48a5dc878343b7ac12a22a4ba6f005ab81672d3b
Ajay-2007 <iit2017040@iiita.ac.in>
2021-08-08 10:00:33 +0530

    added custom markers and expensive operations tests

--- pytest/test-automation-with-pytest/tests.py
+++ pytest/test-automation-with-pytest/tests.py
@@ -9,6 +9,9 @@ from pathlib import Path
 
 
 class ClassTest(object):
+    def expensive_operations(self):
+        time.sleep(1)
+
     @staticmethod
     def test_list_empty_folder():
         """[summary]
@@ -19,8 +22,8 @@ class ClassTest(object):
         try:
             os.mkdir("/tmp/testfolder")
             result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
-            assert (
-                not result.stdout
+            assert not result.stdout.decode(
+                "UTF-8"
             ), "Listing an empty folder did not return expected result!"
         finally:
             shutil.rmtree("/tmp/testfolder")
@@ -37,66 +40,66 @@ class ClassTest(object):
             Path("/tmp/testfolder/first.txt").touch()
             result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
             print("Result: [{}]".format(result))
-            assert "first.txt" in str(
-                result.stdout
+            assert "first.txt" in result.stdout.decode(
+                "UTF-8"
             ), "Listing a folder with one file did not return expected result!"
         finally:
             shutil.rmtree("/tmp/testfolder")
 
-    @staticmethod
-    def test_list_multiple_files():
+    def test_list_multiple_files(self):
         """test simple ls
 
         Returns:
             [type]: [description]
         """
+        self.expensive_operations()
         try:
             os.mkdir("/tmp/testfolder")
             Path("/tmp/testfolder/first.txt").touch()
             Path("/tmp/testfolder/second.doc").touch()
             result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
             print("Result: [{}]".format(result))
-            assert "first.txt" in str(
-                result.stdout
+            assert "first.txt" in result.stdout.decode(
+                "UTF-8"
             ), "Listing a folder with multiple files did not return expected result!"
-            assert "second.doc" in str(
-                result.stdout
+            assert "second.doc" in result.stdout.decode(
+                "UTF-8"
             ), "Listing a folder with multiple files did not return expected result!"
         finally:
             shutil.rmtree("/tmp/testfolder")
 
-    @staticmethod
-    def test_hidden_files():
+    def test_hidden_files(self):
         """test simple ls
 
         Returns:
             [type]: [description]
         """
+        self.expensive_operations()
         try:
             os.mkdir("/tmp/testfolder")
             Path("/tmp/testfolder/first.txt").touch()
             Path("/tmp/testfolder/.hidden_file").touch()
             result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
             print("Result: [{}]".format(result))
-            assert "first.txt" in str(
-                result.stdout
+            assert "first.txt" in result.stdout.decode(
+                "UTF-8"
             ), "Listing a folder with hidden file did not return expected result!"
-            assert ".hidden_file" not in str(
-                result.stdout
+            assert ".hidden_file" not in result.stdout.decode(
+                "UTF-8"
             ), "ls listed hidden file when it shouldn't have!"
         finally:
             shutil.rmtree("/tmp/testfolder")
 
-    @staticmethod
     @pytest.mark.skipif(
         sys.platform.startswith("win"), reason="Skipping non-windows test"
     )
-    def test_list_hidden_files():
+    def test_list_hidden_files(self):
         """test simple ls
 
         Returns:
             [type]: [description]
         """
+        self.expensive_operations()
         try:
             os.mkdir("/tmp/testfolder")
             Path("/tmp/testfolder/first.txt").touch()
@@ -105,32 +108,17 @@ class ClassTest(object):
                 ["ls", "-a", "/tmp/testfolder"], stdout=subprocess.PIPE
             )
             print("Result: [{}]".format(result))
-            assert "first.txt" in str(
-                result.stdout
+            assert "first.txt" in result.stdout.decode(
+                "UTF-8"
             ), "Listing a folder with hidden file did not return expected result!"
-            assert ".hidden_file" in str(
-                result.stdout
+            assert ".hidden_file" in result.stdout.decode(
+                "UTF-8"
             ), "ls listed hidden file when it shouldn't have!"
         finally:
             shutil.rmtree("/tmp/testfolder")
 
     @staticmethod
-    @pytest.mark.skipif(
-        not sys.platform.startswith("win"), reason="Skipping windows-only test"
-    )
-    def test_ls_windows():
-        try:
-            os.mkdir("c:\testfolder")
-            Path("c:\testfolder\first.txt").touch()
-            result = subprocess.run(["ls", "c:\testfolder"], stdout=subprocess.PIPE)
-            print("Result: [{}]".format(result))
-            assert "first.txt" in str(
-                result.stdout
-            ), "Listing a folder with one file did not return expected result!"
-        finally:
-            shutil.rmtree("c:\testfolder")
-
-    @staticmethod
+    @pytest.mark.notpassing
     @pytest.mark.xfail(reason="-y parameter does not yet work")
     def test_unimplemented_feature():
         """test simple ls
@@ -145,20 +133,20 @@ class ClassTest(object):
                 ["ls", "-y", "/tmp/testfolder"], stdout=subprocess.PIPE
             )
             print("Result: [{}]".format(result))
-            assert "first.txt" in str(
-                result.stdout
+            assert "first.txt" in result.stdout.decode(
+                "UTF-8"
             ), "Listing a folder with -y option did not return expected result!"
         finally:
             shutil.rmtree("/tmp/testfolder")
 
-    @staticmethod
     @pytest.mark.parametrize("argument", ["", "-r", "-t", "-rt"])
-    def test_order(argument):
+    def test_order(self, argument):
         """test simple ls
 
         Returns:
             [type]: [description]
         """
+        self.expensive_operations()
         try:
             os.mkdir("/tmp/testfolder")
             Path("/tmp/testfolder/first.txt").touch()
@@ -171,7 +159,26 @@ class ClassTest(object):
             result = subprocess.run(shlex.split(arguments), stdout=subprocess.PIPE)
             assert result.stdout.decode("UTF-8").startswith(
                 "first.txt" if argument in ["", "-rt"] else "second.txt"
-            ), "output of ls with -rt argument was wrong!"
+            ), "output of ls with argument '{}' was wrong!".format(argument)
 
         finally:
             shutil.rmtree("/tmp/testfolder")
+
+
+@pytest.mark.skipif(
+    not sys.platform.startswith("win"), reason="Skipping windows-only test"
+)
+class WindowsTest(object):
+    @staticmethod
+    @pytest.mark.notpassing
+    def test_ls_windows():
+        try:
+            os.mkdir("c:\testfolder")
+            Path("c:\testfolder\first.txt").touch()
+            result = subprocess.run(["ls", "c:\testfolder"], stdout=subprocess.PIPE)
+            print("Result: [{}]".format(result))
+            assert "first.txt" in result.stdout.decode(
+                "UTF-8"
+            ), "Listing a folder with one file did not return expected result!"
+        finally:
+            shutil.rmtree("c:\testfolder")
